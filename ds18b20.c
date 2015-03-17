2df9f53277315b1a445d6e8dfb2997df65d287ab
David Fryer <dfryer1193@gmail.com>
2015-03-17 10:07:33 -0400

    Contains function code for basic control over the ds18b20.

--- ds18b20.c
+++ ds18b20.c
@@ -1,12 +1,46 @@
 #include "ds18b20.h"
 
-uint8_t ds18b20::reset(void) { 
-  uint16_t mask = bitmask;
-  volatile uint8_t *reg IO_REG_ASM = baseReg;
-  uint8_t r;
-  uint8_t retries = 125;
+/*
+ * Reads the temperature from the device.
+ * ( deg c )
+ */
+float getTemp(void) {
+  uint8_t busy = 0;
+  uint8_t temp1;
+  uint8_t temp2;
+  signed int16_t temp3;
+  float result;
 
-  //TODO define macro for setting output
-  do {
-  } while (TRUE);
+  // Address all devices
+  skip_rom();
+  write_byte(CONVERT_T_COMMAND); // Start temperature conversion
+
+  while(!busy) {        // Wait while bus is low
+    busy = read_byte();
+  }
+
+  reset();
+  write_byte(READ_SCRATCHPAD_COMMAND);
+  temp1 = read_byte();
+  temp2 = read_byte();
+  temp3 = make16(temp2, temp1);
+
+  // Temp conversion
+  result = (float) temp3 / 16.0;
+
+  // Expire the time slot
+  _delay_ms(200);
+  return result; // This is in deg c
+}
+
+/*
+ * Args: alarm trigger high, alarm trigger low, config
+ */
+void ds18b20_configure(uint8_t tH, uint8_t tL, uint8_t conf) {
+  reset();
+  skip_rom();
+  write_byte(WRITE_SCRATCHPAD_COMMAND); // Write to scratchpad
+  write_byte(tH);
+  write_byte(tL);
+  write_byte(conf);
 }
